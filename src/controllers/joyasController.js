644cf3c0eb5892439053c98ae4b9b2abe4bb1f9e
Javier Garin <dev.javiergarin@gmail.com>
2025-02-08 12:51:56 -0300

    update files

--- src/controllers/joyasController.js
+++ src/controllers/joyasController.js
@@ -2,11 +2,11 @@ const joyasService = require('../services/joyasService');
 
 const getJoyas = async (req, res) => {
     try {
-        const { limits, page, order_by } = res.query;
+        const { limits, page, order_by } = req.query;
         const joyas = await joyasService.getJoyas(limits, page, order_by);
         res.json(joyas);
-    }   catch (error) {
-        res.status(500).json({ error: error.message});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 };
 
@@ -16,9 +16,8 @@ const getJoyasFiltradas = async (req, res) => {
         const joyas = await joyasService.getJoyasFiltrada(precio_max, precio_min, categoria, metal);
         res.json(joyas);
     } catch (error) {
-        res.status(500).json({ error: error.message});
+        res.status(500).json({ error: error.message });
     }
-
 };
 
 module.exports = { getJoyas, getJoyasFiltradas };
