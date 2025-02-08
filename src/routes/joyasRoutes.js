5878da8a4f7e06c772df34941eaf1f9fe8ef5462
Javier Garin <dev.javiergarin@gmail.com>
2025-02-08 12:50:04 -0300

    update files

--- src/routes/joyasRoutes.js
+++ src/routes/joyasRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const joyasController = require('../controllers/joyasController');
 
-router.get('/', joyasController.getJoyasFiltradas);
+router.get('/', joyasController.getJoyas);
 router.get('/filtros', joyasController.getJoyasFiltradas);
 
 module.exports = router;
