077897203d53c2cd938a8282ec8173a156b3db29
Javier Garin <dev.javiergarin@gmail.com>
2025-02-09 16:11:02 -0300

    update files

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'joyas',
-    password: '12251225',
+    user: "postgres",
+    host: "localhost",
+    database: "joyas",
+    password: "12251225",
     port: 5432,
 });
 
